31ec2cd28f812c6bf0b564e0b628e559e060f21a
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-21 16:38:43 +0100

    Test

---
+++ src/Containers/ThemeContainer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Theme\Containers;
+
+use Plenty\Plugin\Templates\Twig;
+
+class ThemeContainer
+{
+	public function call(Twig $twig):string
+	{
+		return $twig->render('Theme::content.Theme');
+	}
+}
+
+?>
---
+++ src/Providers/ThemeServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Theme\Providers;
+
+use Plenty\Plugin\ServiceProvider;
+
+class ThemeServiceProvider extends ServiceProvider
+{
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+
+	}
+}
+
+?>
