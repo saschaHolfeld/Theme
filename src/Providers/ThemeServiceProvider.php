7ec6577fdd7638282e33424e3738c359651b3439
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-22 10:07:58 +0100

    test234

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace Theme\Providers;
 
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class ThemeServiceProvider extends ServiceProvider
 {
@@ -14,6 +17,16 @@ class ThemeServiceProvider extends ServiceProvider
 	{
 
 	}
-}
 
+	/**
+	 * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+	 */
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)
+	{
+		$eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+		{
+			$partial->set('footer', 'Theme::content.ThemeFooter');
+		}, 99);
+	}
+}
 ?>
