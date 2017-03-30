25a2abe8d7845bbb308342a55b87c945c3adac26
saschaHolfeld <sascha.holfeld@3wfuture.de>
2017-03-30 15:47:14 +0200

    test new items 03301547

--- src/Containers/ThemeContainer.php
+++ src/Containers/ThemeContainer.php
@@ -11,12 +11,14 @@ class ThemeContainer extends Controller
 	public function call(Twig $twig, ItemDataLayerRepositoryContract $itemRepository):string
 	{
 		$topItems = self::showTopItems($itemRepository);
+		$newItems = self::showNewItems($itemRepository);
 
 		$templateData = array(
 				'topItems' => $topItems,
+				'newItems' => $newItems,
 		);
 
-		return $twig->render('Theme::content.Theme', $topItems);
+		return $twig->render('Theme::content.Theme', $templateData);
 	}
 
 
@@ -42,6 +44,46 @@ class ThemeContainer extends Controller
 		$itemFilter = [
 				'itemBase.isStoreSpecial' => [
 						'shopAction' => [3]
+				]
+		];
+
+		$itemParams = [
+				'language' => 'de'
+		];
+
+		$resultItems = $itemRepository->search($itemColumns, $itemFilter, $itemParams);
+
+		$items = array();
+		foreach ($resultItems as $item)
+		{
+			$items[] = $item;
+		}
+
+		return $items;
+	}
+
+	public static function showNewItems(ItemDataLayerRepositoryContract $itemRepository):array
+	{
+		$itemColumns = [
+				'itemDescription' => [
+						'name1',
+						'description'
+				],
+				'variationBase' => [
+						'id'
+				],
+				'variationRetailPrice' => [
+						'price'
+				],
+				'variationImageList' => [
+						'path',
+						'cleanImageName'
+				]
+		];
+
+		$itemFilter = [
+				'itemBase.isStoreSpecial' => [
+						'shopAction' => [1]
 				]
 		];
 
